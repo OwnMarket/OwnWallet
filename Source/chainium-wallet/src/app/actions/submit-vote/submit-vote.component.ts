04b50d67df952d0310ba55a6cc8a8fbc062eabd0
Darko Suljic <darko.suljic@weown.com>
2019-03-13 12:12:00 +0100

    add submit vote component

---
+++ Source/chainium-wallet/src/app/actions/submit-vote/submit-vote.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { SubmitVote } from "src/app/models/SubmitTransactions";
+import { ActionRemoval } from "src/app/services/ActionRemovalService";
+
+@Component({
+  selector: "app-submit-vote",
+  templateUrl: "./submit-vote.component.html",
+  styleUrls: ["./submit-vote.component.scss"]
+})
+
+export class SubmitVoteComponent implements OnInit {
+  
+  @Input() submitVote : SubmitVote;
+  @Input() removalService : ActionRemoval;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRemoveSubmitVoteClick(){
+    if(this.removalService){
+      this.removalService.removeAction(this.submitVote);
+    }
+  }
+}

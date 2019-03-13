0923839499c662808be35f34774e238789acaf4a
Darko Suljic <darko.suljic@weown.com>
2019-03-13 12:09:56 +0100

    added create account component

---
+++ Source/chainium-wallet/src/app/actions/create-account/create-account.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { CreateAccount } from "src/app/models/SubmitTransactions";
+import { ActionRemoval } from "src/app/services/ActionRemovalService";
+
+@Component({
+  selector: "app-create-account",
+  templateUrl: "./create-account.component.html",
+  styleUrls: ["./create-account.component.scss"]
+})
+
+export class CreateAccountComponent implements OnInit {
+  
+  @Input() createAccount : CreateAccount;
+  @Input() removalService : ActionRemoval;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRemoveCreateAccountClick(){
+    if(this.removalService){
+      this.removalService.removeAction(this.createAccount);
+    }
+  }
+}

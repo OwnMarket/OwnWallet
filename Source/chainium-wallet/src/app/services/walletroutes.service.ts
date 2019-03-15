535eb210cbae137e3b41ffe368c6711d32021fd8
Darko Suljic <darko.suljic@weown.com>
2019-03-15 07:41:12 +0100

    validator info under block info in menu

--- Source/chainium-wallet/src/app/services/walletroutes.service.ts
+++ Source/chainium-wallet/src/app/services/walletroutes.service.ts
@@ -6,12 +6,12 @@ const ROUTES: WalletRouteInfo[] = [
   { route: 'importwallet', linkText: 'Import wallet', icon: 'account_balance_wallet' },
   { route: 'addressinfo', linkText: 'Address info', icon: 'markunread_mailbox' },
   { route: 'assetInfo', linkText: 'Asset info', icon: 'insert_drive_file' },
-  { route: 'validatorInfo', linkText: 'Validator info', icon: 'gavel' },
   //{ route: 'generateaccount', linkText: 'Generate account', icon: 'how_to_reg' },
   { route: 'accountinfo', linkText: 'Account info', icon: 'person' },
   { route: 'submissions', linkText: 'Submit transaction', icon: 'next_week' },
   { route: 'transaction', linkText: 'Transaction info', icon: 'import_export' },
-  { route: 'block', linkText: 'Block info', icon: 'widgets ' }
+  { route: 'block', linkText: 'Block info', icon: 'widgets ' },
+  { route: 'validatorInfo', linkText: 'Validator info', icon: 'gavel' }
 ];
 
 @Injectable({

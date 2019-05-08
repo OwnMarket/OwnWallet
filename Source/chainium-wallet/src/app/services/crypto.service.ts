71a4a38a1f2de4ef44e93e7573f5fd3492d6f5f4
Elvir Tabakovic <elvir.tabakovic.92@gmail.com>
2019-05-08 10:38:47 +0200

    update own sdk lib

--- Source/chainium-wallet/src/app/services/crypto.service.ts
+++ Source/chainium-wallet/src/app/services/crypto.service.ts
@@ -5,32 +5,32 @@ import { environment } from '../../environments/environment';
 import { WalletInfo } from '../models/wallet-info.model';
 import { Tx, TxEnvelope } from '../models/submit-transactions.model';
 
-declare var chainiumSdk: any;
+declare var ownBlockchainSdk: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
   public generateWallet(): Observable<WalletInfo> {
-    const wallet = chainiumSdk.crypto.generateWallet();
+    const wallet = ownBlockchainSdk.crypto.generateWallet();
     return of(wallet);
   }
 
   public getAddressFromKey(privateKey: string): Observable<any> {
-    const address = chainiumSdk.crypto.addressFromPrivateKey(privateKey);
+    const address = ownBlockchainSdk.crypto.addressFromPrivateKey(privateKey);
     return of(address);
   }
 
   public signTransaction(privateKey: string, tx: Tx): Observable<TxEnvelope> {
-    const rawTx = chainiumSdk.crypto.utf8ToHex(JSON.stringify(tx));
-    const signature = chainiumSdk.crypto.signMessage(environment.networkCode,privateKey, rawTx);
+    const rawTx = ownBlockchainSdk.crypto.utf8ToHex(JSON.stringify(tx));
+    const signature = ownBlockchainSdk.crypto.signMessage(environment.networkCode,privateKey, rawTx);
     return of({
-      tx: chainiumSdk.crypto.encode64(rawTx),
+      tx: ownBlockchainSdk.crypto.encode64(rawTx),
       signature: signature
     });
   }
 
   public deriveHash(address: string, nonce: number, txActionNumber: number): string {
-    return chainiumSdk.crypto.deriveHash(address, nonce, txActionNumber);
+    return ownBlockchainSdk.crypto.deriveHash(address, nonce, txActionNumber);
   }
 }

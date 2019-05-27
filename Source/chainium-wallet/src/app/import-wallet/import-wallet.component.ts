379a4c2607ddbd788b07325f6333c2764688ce63
Marian Gheorghe <marian.gheorghe@chainium.io>
2019-05-27 18:31:21 +0200

    More disclaimers (restore wallet)

--- Source/chainium-wallet/src/app/import-wallet/import-wallet.component.ts
+++ Source/chainium-wallet/src/app/import-wallet/import-wallet.component.ts
@@ -11,7 +11,6 @@ import { WalletService } from '../services/wallet.service';
 export class ImportWalletComponent implements OnInit {
     hide : boolean;
     privateKey : string;
-    errors: string[];
     constructor(private privateKeyService : PrivatekeyService, 
         private cryptoService : CryptoService,
         private walletService : WalletService) {
@@ -31,28 +30,12 @@ export class ImportWalletComponent implements OnInit {
                 privateKey : this.privateKey,
                 address : (address as string)
             });
-
-            this.displayErrors(address);            
+   
             this.privateKeyService.sendMessage(this.privateKeyService.existsKey());
         });     
     }
 
-    ngOnInit() {
-        this.privateKeyService.getMessage().subscribe(msg => {
-            let walletInfo = this.privateKeyService.getWalletInfo();
-            if (walletInfo) 
-                this.displayErrors(walletInfo.address);
-        });
-    }
-
-    private displayErrors(address: string) {
-        let chxAddresses = this.walletService.getAllChxAddresses();
-        if (chxAddresses.indexOf(address) == -1){
-            this.errors = ["Imported key is not recoverable. Your PK will be unloaded from the wallet upon closing the application or clicking the Unload Private Key button"];
-        }
-        else {
-            this.errors = null;
-        }
+    ngOnInit() {        
     }
 }
 

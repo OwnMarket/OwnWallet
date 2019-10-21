d4bd5e7e5cf0a2013132cbb4f645b7b456281dd0
SKaDiZZ <akirapowered@gmail.com>
2019-10-21 21:29:42 +0200

    updated adress info, block info, transaction info

--- Source/chainium-wallet/src/app/info/block-info/block-info.component.ts
+++ Source/chainium-wallet/src/app/info/block-info/block-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { BlockInfo } from 'src/app/models/block-info.model';
@@ -10,15 +10,18 @@ import { NodeService } from 'src/app/services/node.service';
   styleUrls: ['./block-info.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class BlockInfoComponent implements OnInit {
+export class BlockInfoComponent implements OnInit, OnDestroy {
 
   blockNumber = 0;
   blockInfo: BlockInfo;
   subscription: Subscription;
   errors: string[];
   showStakingRewards: boolean;
-  constructor(private nodeService: NodeService,
-    private route: ActivatedRoute) { }
+
+  constructor(
+    private nodeService: NodeService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.subscription = this.route.params.subscribe(params => {
@@ -30,18 +33,22 @@ export class BlockInfoComponent implements OnInit {
           if (latestBlockNo && !isNaN(Number.parseInt(latestBlockNo))) {
             this.blockNumber = latestBlockNo;
           }
-        })
-      }
-      else
+        });
+      } else {
         this.blockNumber = blockNo;
+      }
 
       this.onBlockInfoButtonClick();
     });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   onBlockInfoButtonClick() {
-    if (this.blockNumber < 0 || this.blockNumber === null)
-      return;
+
+    if (this.blockNumber < 0 || this.blockNumber === null) { return; }
 
     this.nodeService.getBlockInfo(this.blockNumber).subscribe(info => {
       if (!info || info.errors) {
@@ -49,13 +56,16 @@ export class BlockInfoComponent implements OnInit {
         this.errors = info.errors;
         return;
       }
-      let timestamp = info.timestamp > 2 ** 32 ? info.timestamp : info.timestamp * 1000; // New version in milliseconds.
-      if (timestamp === 0)
+
+      const timestamp = info.timestamp > 2 ** 32 ? info.timestamp : info.timestamp * 1000; // New version in milliseconds.
+
+      if (timestamp === 0) {
         info.blockTime = '';
-      else {
+      } else {
         info.blockTime = new Date(timestamp).toISOString(); // Block timestamp is Unix time.
         info.timestamp = `(${info.timestamp})`;
       }
+
       this.errors = null;
       this.blockInfo = info as BlockInfo;
       this.showStakingRewards =

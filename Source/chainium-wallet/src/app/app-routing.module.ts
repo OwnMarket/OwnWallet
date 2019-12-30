d4846a972134fccac4203a472312f6f475e6d402
SKaDiZZ <akirapowered@gmail.com>
2019-12-30 14:45:14 +0100

    removed differential loading on wallet and info module

--- Source/chainium-wallet/src/app/app-routing.module.ts
+++ Source/chainium-wallet/src/app/app-routing.module.ts
@@ -1,46 +1,149 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RecoverPkFromOldDerivationPathComponent } from './recover-pk-from-old-derivation-path/recover-pk-from-old-derivation-path.component';
-import { SubmitTransactionComponent } from './submit-transaction/submit-transaction.component';
-import { GenerateAccountComponent } from './generate-account/generate-account.component';
 import { LoginComponent } from './login/login.component';
 import { AccessGuard } from './_guards/access.guard';
 
+// Info section
+import { InfoPageComponent } from './info/info-page/info-page.component';
+import { AddressInfoComponent } from './info/address-info/address-info.component';
+import { AccountInfoComponent } from './info/account-info/account-info.component';
+import { AssetInfoComponent } from './info/asset-info/asset-info.component';
+import { TransactionInfoComponent } from './info/transaction-info/transaction-info.component';
+import { BlockInfoComponent } from './info/block-info/block-info.component';
+import { ValidatorInfoComponent } from './info/validator-info/validator-info.component';
+import { EquivocationProofInfoComponent } from './info/equivocation-proof-info/equivocation-proof-info.component';
+
+// Wallet section
+import { WelcomeComponent } from './wallet/welcome/welcome.component';
+import { ImportComponent } from './wallet/import/import.component';
+import { CreateComponent } from './wallet/create/create.component';
+import { SendChxComponent } from './wallet/actions/send-chx/send-chx.component';
+import { ReceiveChxComponent } from './wallet/actions/receive-chx/receive-chx.component';
+import { ValidatorManagmentComponent } from './wallet/validator-managment/validator-managment.component';
+import { StakingComponent } from './wallet/staking/staking.component';
+import { MessageSignVerificationComponent } from './wallet/msg-sign-verify/msg-sign-verify.component';
 
 export const ROUTERCONFIGS: Routes = [
-/*
-    {
-        path: 'wallet',
-        component: PrivateComponent,
-        // canActivate: [AccessGuard],
-        children: [
-            { path: 'create-wallet', component: NewWalletComponent},
-            { path: 'restore-wallet', component: RestoreWalletComponent},
-            { path: 'unload-wallet', component: UnloadWalletComponent},
-            { path: 'import-wallet', component: ImportWalletComponent },
-            { path: 'submit-tx', component: SubmitTransactionComponent },
-            { path: 'home', component: HomeScreenComponent, canActivate: [AccessGuard] },
-            { path: 'validator', component: ValidatorInfoComponent },
-            { path: 'validator/:validatorHash', component: ValidatorInfoComponent },
-            { path: 'equivocation/:equivocationProofHash', component: EquivocationProofInfoComponent },
-            //{ path: 'generateaccount', component: GenerateAccountComponent },
-            { path: 'sign-verify', component: MessageSignVerificationComponent },
-            { path: 'recover-pk-from-old-derivation-path', component: RecoverPkFromOldDerivationPathComponent }
-        ]
-    }, */
     {
       path: 'login',
       component: LoginComponent
     },
     {
       path: 'info',
-      loadChildren: './info/info.module#InfoModule'
+      component: InfoPageComponent,
+      children: [
+        {
+          path: '',
+          redirectTo: 'block',
+          pathMatch: 'full'
+        },
+        {
+          path: 'address',
+          component: AddressInfoComponent,
+          data: { state: 'address' }
+        },
+        {
+          path: 'address/:addressHash',
+          component: AddressInfoComponent,
+          data: { state: 'address' }
+        },
+        {
+          path: 'account',
+          component: AccountInfoComponent,
+          data: { state: 'account' }
+        },
+        {
+          path: 'account/:accountHash',
+          component: AccountInfoComponent,
+          data: { state: 'account' }
+        },
+        {
+          path: 'asset',
+          component: AssetInfoComponent,
+          data: { state: 'asset' }
+        },
+        {
+          path: 'asset/:assetHash',
+          component: AssetInfoComponent,
+          data: { state: 'asset' }
+        },
+        {
+          path: 'tx',
+          component: TransactionInfoComponent,
+          data: { state: 'tx' }
+        },
+        {
+          path: 'tx/:transactionHash',
+          component: TransactionInfoComponent,
+          data: { state: 'tx' }
+        },
+        {
+          path: 'block',
+          component: BlockInfoComponent,
+          data: { state: 'block' }
+        },
+        {
+          path: 'block/:blockNumber',
+          component: BlockInfoComponent,
+          data: { state: 'block' }
+        },
+        {
+           path: 'validator',
+           component: ValidatorInfoComponent,
+           data: { state: 'validator' }
+        },
+        {
+          path: 'validator/:validatorHash',
+          component: ValidatorInfoComponent,
+          data: { state: 'validator' }
+        },
+        {
+          path: 'equivocation/:equivocationProofHash',
+          component: EquivocationProofInfoComponent
+        }
+      ]
     },
     {
       path: 'wallet',
-      loadChildren: './wallet/wallet.module#WalletModule',
-      canActivate: [AccessGuard]
+      canActivate: [AccessGuard],
+      component: WelcomeComponent,
+      children: [
+        {
+          path: 'create',
+          component: CreateComponent,
+          data: { state: 'create' }
+        },
+        {
+          path: 'import',
+          component: ImportComponent,
+          data: { state: 'import' }
+        },
+        {
+          path: 'send-chx',
+          component: SendChxComponent,
+          data: { state: 'send-chx' }
+        },
+        {
+          path: 'receive-chx',
+          component: ReceiveChxComponent,
+          data: { state: 'receive-chx' }
+        },
+        {
+          path: 'validator-managment',
+          component: ValidatorManagmentComponent,
+          data: { state: 'validator-managment' }
+        },
+        {
+          path: 'staking',
+          component: StakingComponent,
+          data: { state: 'staking' }
+        },
+        {
+          path: 'sign-verify',
+          component: MessageSignVerificationComponent
+        }
+      ]
     },
     {
       path: '',
--- Source/chainium-wallet/src/app/app.module.ts
+++ Source/chainium-wallet/src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { WalletHttpInterceptor } from './services/wallet-http-interceptor';
+import { GlobalErrorHandler } from './services/global.error.handler';
 import { GoogleMaterialModule } from './/google-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './/app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ClipboardModule } from 'ngx-clipboard';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SharedModule } from './shared/shared.module';
+import { QrCodeModule } from 'ng-qrcode';
+import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { RecoverPkFromOldDerivationPathComponent } from './recover-pk-from-old-derivation-path/recover-pk-from-old-derivation-path.component';
@@ -12,11 +18,8 @@ import { SubmitTransactionComponent } from './submit-transaction/submit-transact
 import { ChxTransferComponent } from './actions/chx-transfer/chx-transfer.component';
 import { AssetTransferComponent } from './actions/asset-transfer/asset-transfer.component';
 import { SubmitTransactionInfoComponent } from './submit-transaction-info/submit-transaction-info.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { GlobalErrorHandler } from './services/global.error.handler';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 import { GenerateAccountComponent } from './generate-account/generate-account.component';
-import { WalletHttpInterceptor } from './services/wallet-http-interceptor';
 import { LoaderComponent } from './loader/loader.component';
 import { DelegateStakeComponent } from './actions/delegate-stake/delegate-stake.component';
 import { CreateAssetEmissionComponent } from './actions/create-asset-emission/create-asset-emission.component';
@@ -25,11 +28,30 @@ import { SetAssetControllerComponent } from './actions/set-asset-controller/set-
 import { SetAccountControllerComponent } from './actions/set-account-controller/set-account-controller.component';
 import { SubmitVoteComponent } from './actions/submit-vote/submit-vote.component';
 import { SubmitVoteWeightComponent } from './actions/submit-vote-weight/submit-vote-weight.component';
-import { FormsModule } from '@angular/forms';
 import { CreateAssetComponent } from './actions/create-asset/create-asset.component';
 import { CreateAccountComponent } from './actions/create-account/create-account.component';
+
 import { LoginComponent } from './login/login.component';
 
+// Info section
+import { InfoPageComponent } from './info/info-page/info-page.component';
+import { AddressInfoComponent } from './info/address-info/address-info.component';
+import { AccountInfoComponent } from './info/account-info/account-info.component';
+import { AssetInfoComponent } from './info/asset-info/asset-info.component';
+import { TransactionInfoComponent } from './info/transaction-info/transaction-info.component';
+import { BlockInfoComponent } from './info/block-info/block-info.component';
+import { ValidatorInfoComponent } from './info/validator-info/validator-info.component';
+import { EquivocationProofInfoComponent } from './info/equivocation-proof-info/equivocation-proof-info.component';
+
+// Wallet section
+import { WelcomeComponent } from './wallet/welcome/welcome.component';
+import { ImportComponent } from './wallet/import/import.component';
+import { CreateComponent } from './wallet/create/create.component';
+import { SendChxComponent } from './wallet/actions/send-chx/send-chx.component';
+import { ReceiveChxComponent } from './wallet/actions/receive-chx/receive-chx.component';
+import { ValidatorManagmentComponent } from './wallet/validator-managment/validator-managment.component';
+import { StakingComponent } from './wallet/staking/staking.component';
+import { MessageSignVerificationComponent } from './wallet/msg-sign-verify/msg-sign-verify.component';
 
 @NgModule({
     entryComponents: [
@@ -56,16 +78,37 @@ import { LoginComponent } from './login/login.component';
         ErrorDialogComponent,
         GenerateAccountComponent,
         LoaderComponent,
-        LoginComponent
+        LoginComponent,
+        // info section
+        InfoPageComponent,
+        AddressInfoComponent,
+        AccountInfoComponent,
+        AssetInfoComponent,
+        TransactionInfoComponent,
+        BlockInfoComponent,
+        ValidatorInfoComponent,
+        EquivocationProofInfoComponent,
+        // wallet section
+        CreateComponent,
+        ImportComponent,
+        WelcomeComponent,
+        SendChxComponent,
+        ReceiveChxComponent,
+        ValidatorManagmentComponent,
+        StakingComponent,
+        MessageSignVerificationComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         SharedModule,
         GoogleMaterialModule,
         ClipboardModule,
+        NgxDatatableModule,
+        QrCodeModule,
         AppRoutingModule,
     ],
     providers: [

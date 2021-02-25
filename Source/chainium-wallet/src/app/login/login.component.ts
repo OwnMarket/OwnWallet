52c41ffc61c0611d50115ad4800b3694eb4d673a
SKaDiZZ <akirapowered@gmail.com>
2021-02-25 12:14:18 +0100

    updated login page added caps lock indicator

--- Source/chainium-wallet/src/app/login/login.component.ts
+++ Source/chainium-wallet/src/app/login/login.component.ts
@@ -7,71 +7,78 @@ import { CryptoService } from 'src/app/shared/services/crypto.service';
 import { WalletService } from 'src/app/shared/services/wallet.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
-
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  needPasswordOnly: boolean;
+  walletKeystore: string;
+  hide: boolean;
+  showRestore: boolean;
+  capsLockStatus: boolean;
 
-    needPasswordOnly: boolean;
-    walletKeystore: string;
-    hide: boolean;
-    showRestore: boolean;
+  password = new FormControl('', [Validators.required]);
+  saveKeystore: boolean;
 
-    password = new FormControl('', [Validators.required]);
-    saveKeystore: boolean;
+  constructor(
+    private router: Router,
+    private cryptoService: CryptoService,
+    private walletService: WalletService,
+    private ownModalService: OwnModalService
+  ) {
+    this.hide = true;
+    this.saveKeystore = true;
+    this.needPasswordOnly = false;
+    this.showRestore = false;
+  }
 
+  ngOnInit() {
+    const walletContext = this.walletService.getWalletContext();
+    if (walletContext.walletKeystore) {
+      this.walletKeystore = walletContext.walletKeystore;
+      this.needPasswordOnly = true;
+    } else {
+      this.router.navigate(['/wallet']);
+    }
+  }
 
-    constructor(
-        private router: Router,
-        private cryptoService: CryptoService,
-        private walletService: WalletService,
-        private ownModalService: OwnModalService
-        ) {
-            this.hide = true;
-            this.saveKeystore = true;
-            this.needPasswordOnly = false;
-            this.showRestore = false;
+  onKeyDown(event: KeyboardEvent) {
+    if (event && event.key === 'Enter') {
+      this.onSubmitPassword();
+    }
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    if (event.getModifierState('CapsLock')) {
+      this.capsLockStatus = true;
+    } else {
+      this.capsLockStatus = false;
     }
 
-    ngOnInit() {
-        const walletContext = this.walletService.getWalletContext();
-        if (walletContext.walletKeystore) {
-            this.walletKeystore = walletContext.walletKeystore;
-            this.needPasswordOnly = true;
-        } else {
-            this.router.navigate(['/wallet']);
-        }
+    if (event.key === 'CapsLock') {
+      this.capsLockStatus = !this.capsLockStatus;
     }
+  }
 
-    onKeyDown(event: KeyboardEvent) {
-        if (event && event.key === 'Enter') {
-            this.onSubmitPassword();
-        }
+  onSubmitPassword() {
+    this.password.markAsTouched();
+    if (this.password.valid && this.walletKeystore) {
+      const passwordHash = this.cryptoService.hash(this.password.value);
+      const walletContext = { walletKeystore: this.walletKeystore, passwordHash };
+      try {
+        this.cryptoService
+          .generateWalletFromKeystore(walletContext.walletKeystore, walletContext.passwordHash, 0)
+          .subscribe((w) => {});
+        this.walletService.setWalletContext(walletContext);
+        this.router.navigate(['/wallet']);
+      } catch {
+        this.password.setErrors({ incorrect: true });
+      }
     }
+  }
 
-    onSubmitPassword() {
-        this.password.markAsTouched();
-        if (this.password.valid && this.walletKeystore) {
-            const passwordHash = this.cryptoService.hash(this.password.value);
-            const walletContext = { walletKeystore: this.walletKeystore, passwordHash };
-            try {
-                this.cryptoService.generateWalletFromKeystore(
-                    walletContext.walletKeystore,
-                    walletContext.passwordHash,
-                    0
-                ).subscribe(w => {});
-                this.walletService.setWalletContext(walletContext);
-                this.router.navigate(['/wallet']);
-            } catch {
-                this.password.setErrors({'incorrect': true});
-            }
-        }
-    }
-
-    openModal(id: string) {
-      this.ownModalService.open(id);
-    }
-    
+  openModal(id: string) {
+    this.ownModalService.open(id);
+  }
 }

3a22c06012cd509edff0e578ecf8f066f98a6ad8
SKaDiZZ <akirapowered@gmail.com>
2021-03-08 20:49:12 +0100

    show error message when importing private key if private key is invalid or contains invalid characters

--- Source/chainium-wallet/src/app/wallet/import/import.component.ts
+++ Source/chainium-wallet/src/app/wallet/import/import.component.ts
@@ -14,10 +14,9 @@ import { OwnAnimations } from '../../shared';
   selector: 'app-import',
   templateUrl: './import.component.html',
   styleUrls: ['./import.component.css'],
-  animations: [OwnAnimations.contentInOut]
+  animations: [OwnAnimations.contentInOut],
 })
 export class ImportComponent implements OnInit {
-
   method = 'phrase';
   registerForm: FormGroup;
   mnemonic = new FormControl('', [Validators.required]);
@@ -25,116 +24,129 @@ export class ImportComponent implements OnInit {
   file: any;
   walletKeystore: string;
   privateKey: string;
+  invalidPrivateKey: boolean = false;
 
   hideWithMnemonic: boolean;
   hideWithKeystore: boolean;
   hideWithPrivateKey: boolean;
 
   constructor(
-      private formBuilder: FormBuilder,
-      private router: Router,
-      private privateKeyService: PrivatekeyService,
-      private cryptoService: CryptoService,
-      private walletService: WalletService,
-      private fileService: FileService
-      ) {
-      this.hideWithMnemonic = true;
-      this.hideWithKeystore = true;
-      this.hideWithPrivateKey = true;
-      this.privateKey = '';
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private privateKeyService: PrivatekeyService,
+    private cryptoService: CryptoService,
+    private walletService: WalletService,
+    private fileService: FileService
+  ) {
+    this.hideWithMnemonic = true;
+    this.hideWithKeystore = true;
+    this.hideWithPrivateKey = true;
+    this.privateKey = '';
   }
 
   ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-          password: ['', [Validators.required]],
-          confirmPassword: ['', [Validators.required]],
-          saveKeystore: [true]
-      }, {
-          validator: validatePasswordMatch
-      });
+    this.registerForm = this.formBuilder.group(
+      {
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+        saveKeystore: [true],
+      },
+      {
+        validator: validatePasswordMatch,
+      }
+    );
   }
 
   // Convenience getter for easy access to form fields.
-  get f() { return this.registerForm.controls; }
+  get f() {
+    return this.registerForm.controls;
+  }
 
   fileChanged(e) {
-      // note: For security reasons browsers do not allow getting full path of selected file.
-      this.file = e.target.files[0];
-      const fileReader = new FileReader();
-      fileReader.onload = (event: ProgressEvent) => {
-          this.walletKeystore = fileReader.result.toString();
-      };
-      fileReader.readAsText(this.file);
+    // note: For security reasons browsers do not allow getting full path of selected file.
+    this.file = e.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (event: ProgressEvent) => {
+      this.walletKeystore = fileReader.result.toString();
+    };
+    fileReader.readAsText(this.file);
   }
 
   onRestoreWithFile() {
-      this.walletService.clearWalletContext();
+    this.walletService.clearWalletContext();
 
-      if (this.f.password.valid && this.walletKeystore) {
-          const passwordHash = this.cryptoService.hash(this.f.password.value);
-          const walletContext = { walletKeystore: this.walletKeystore, passwordHash };
-          try {
-              this.cryptoService.generateWalletFromKeystore(
-                  walletContext.walletKeystore,
-                  walletContext.passwordHash,
-                  0
-              ).subscribe(w => {});
-              this.walletService.setWalletContext(walletContext);
-              this.walletService.generateWalletFromContext();
+    if (this.f.password.valid && this.walletKeystore) {
+      const passwordHash = this.cryptoService.hash(this.f.password.value);
+      const walletContext = { walletKeystore: this.walletKeystore, passwordHash };
+      try {
+        this.cryptoService
+          .generateWalletFromKeystore(walletContext.walletKeystore, walletContext.passwordHash, 0)
+          .subscribe((w) => {});
+        this.walletService.setWalletContext(walletContext);
+        this.walletService.generateWalletFromContext();
 
-              setTimeout(() => this.router.navigate(['/wallet']), 100);
-          } catch {
-              this.f.password.setErrors({'incorrect' : true});
-          }
+        setTimeout(() => this.router.navigate(['/wallet']), 100);
+      } catch {
+        this.f.password.setErrors({ incorrect: true });
       }
+    }
   }
 
   onRestoreWithMnemonic() {
-      this.walletService.clearWalletContext();
-      this.mnemonic.markAsTouched();
-      this.f.password.markAsTouched();
-      this.f.confirmPassword.markAsTouched();
+    this.walletService.clearWalletContext();
+    this.mnemonic.markAsTouched();
+    this.f.password.markAsTouched();
+    this.f.confirmPassword.markAsTouched();
 
-      if (this.registerForm.valid && this.mnemonic.valid) {
-          const passwordHash = this.cryptoService.hash(this.f.password.value);
-          try {
-              this.cryptoService.generateWalletKeystore(this.mnemonic.value.trim(), passwordHash)
-              .subscribe((walletKeystore: string) => {
-                  if (this.f.saveKeystore.value) {
-                      this.fileService.saveFile({
-                          fileName: 'wallet-backup.own',
-                          text: walletKeystore
-                      });
-                  }
-                  const walletContext = { walletKeystore, passwordHash };
-                  this.walletService.setWalletContext(walletContext);
-                  this.walletService.generateWalletFromContext();
-
-                  setTimeout(() => this.router.navigate(['/wallet']), 100);
-
+    if (this.registerForm.valid && this.mnemonic.valid) {
+      const passwordHash = this.cryptoService.hash(this.f.password.value);
+      try {
+        this.cryptoService
+          .generateWalletKeystore(this.mnemonic.value.trim(), passwordHash)
+          .subscribe((walletKeystore: string) => {
+            if (this.f.saveKeystore.value) {
+              this.fileService.saveFile({
+                fileName: 'wallet-backup.own',
+                text: walletKeystore,
               });
-          } catch {
-              this.mnemonic.setErrors({'incorrect': true});
-          }
+            }
+            const walletContext = { walletKeystore, passwordHash };
+            this.walletService.setWalletContext(walletContext);
+            this.walletService.generateWalletFromContext();
+
+            setTimeout(() => this.router.navigate(['/wallet']), 100);
+          });
+      } catch {
+        this.mnemonic.setErrors({ incorrect: true });
       }
+    }
   }
 
   onRestoreWithPrivateKey() {
-      this.walletService.clearWalletContext();
+    this.walletService.clearWalletContext();
 
-      if (!this.privateKey) { return; }
+    if (!this.privateKey) {
+      return;
+    }
 
-      this.cryptoService.getAddressFromKey(this.privateKey).subscribe(address => {
+    try {
+      this.cryptoService.getAddressFromKey(this.privateKey).subscribe((address) => {
+        if (!address && !address.errors) {
+          return;
+        }
+        this.privateKeyService.setWalletInfo({
+          privateKey: this.privateKey,
+          address: address as string,
+        });
 
-        if (!address && !address.errors) { return; }
-          this.privateKeyService.setWalletInfo({
-              privateKey : this.privateKey,
-              address : (address as string)
-          });
-
-          this.privateKeyService.sendMessage(this.privateKeyService.existsKey());
-          setTimeout(() => this.router.navigate(['/wallet']), 100);
+        this.privateKeyService.sendMessage(this.privateKeyService.existsKey());
+        setTimeout(() => this.router.navigate(['/wallet']), 100);
       });
+    } catch (error) {
+      this.invalidPrivateKey = true;
+      setTimeout(() => {
+        this.invalidPrivateKey = false;
+      }, 5000);
+    }
   }
-
 }

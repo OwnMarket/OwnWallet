01cf0965d343ba1165976d66f49c7d0e67eea98c
SKaDiZZ <akirapowered@gmail.com>
2021-04-21 18:33:36 +0200

    updated configuration service

--- Source/chainium-wallet/src/app/shared/services/configuration.service.ts
+++ Source/chainium-wallet/src/app/shared/services/configuration.service.ts
@@ -6,8 +6,8 @@ import { prodConfig } from '../configurations/prod.config';
 @Injectable()
 export class ConfigurationService {
   get config() {
-    const config = environment.networkCode === 'OWN_PUBLIC_BLOCKCHAIN_MAINNET' ? prodConfig : testConfig;
-    console.log('is equal', environment.networkCode === 'OWN_PUBLIC_BLOCKCHAIN_MAINNET');
+    const config = environment.networkCode === 'OWN_PUBLIC_BLOCKCHAIN_TESTNET' ? testConfig : prodConfig;
+    console.log('is equal', environment.networkCode === 'OWN_PUBLIC_BLOCKCHAIN_TESTNET');
     console.log('chainId', config['bsc'].chainId);
     return config;
   }

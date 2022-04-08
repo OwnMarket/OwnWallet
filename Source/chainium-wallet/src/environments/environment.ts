5faf3ae6888aae92886a3a94180afb56e9083ab0
SKaDiZZ <akirapowered@gmail.com>
2022-04-08 10:26:25 +0200

    updated environment file

--- Source/chainium-wallet/src/environments/environment.ts
+++ Source/chainium-wallet/src/environments/environment.ts
@@ -1,5 +1,11 @@
-export const environment = {
+/* export const environment = {
   production: false,
   nodeApiUrl: "https://wallet-node.testnet.weown.com",
   networkCode: "OWN_PUBLIC_BLOCKCHAIN_TESTNET",
+}; */
+
+export const environment = {
+  production: false,
+  nodeApiUrl: '<<NODE_API_URL>>',
+  networkCode: '<<NETWORK_CODE>>',
 };
